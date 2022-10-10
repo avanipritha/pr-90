21c9543479f4a7411858fea617095490f1022585
avanipritha <105854907+avanipritha@users.noreply.github.com>
2022-10-10 18:45:33 +0530

    Add files via upload

---
+++ p-89.js
@@ -0,0 +1,9 @@
+function send()
+{
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+}
+document.getElementById("output").innerHTML = row;
+
+
